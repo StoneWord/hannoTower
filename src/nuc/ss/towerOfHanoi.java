e15128efb11f233aea40b697b96030c01cf3c06d
StoneWord <2305489120@qq.com>
2020-11-20 21:38:52 +0800

    汉诺塔问题

---
+++ src/nuc/ss/towerOfHanoi.java
@@ -0,0 +1,25 @@
+package nuc.ss;
+import java.util.Scanner;
+
+public class towerOfHanoi {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter number of disks: ");
+		int n = input.nextInt();
+		input.close();
+		System.out.println("The moves are:");
+		moveDisks(n, 'A', 'B', 'C');
+	}
+	
+	public static void moveDisks(int n, char fromTower, char toTower, char auxTower) {
+		if(n == 1)
+			System.out.println("Move disk "+ n + " form " + fromTower + " to " + toTower);
+		else {
+			moveDisks(n-1 , fromTower , auxTower, toTower);
+			System.out.println("Move disk " + n + " form " + fromTower + " to " + toTower);
+			moveDisks(n-1 , auxTower, toTower, fromTower);
+		}
+		
+	}
+}
